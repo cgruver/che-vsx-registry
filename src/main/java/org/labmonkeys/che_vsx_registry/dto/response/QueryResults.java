b24943962642030450fc27f955e17466d4784ab0
cgruver <cgruver@clg.lab>
2023-07-17 13:15:41 +0000

    wip code stash

---
+++ src/main/java/org/labmonkeys/che_vsx_registry/dto/response/QueryResults.java
@@ -0,0 +1,5 @@
+package org.labmonkeys.che_vsx_registry.dto.response;
+
+import java.util.List;
+
+public record QueryResults(List<QueryResponseDto> results ) {}
--- src/main/java/org/labmonkeys/che_vsx_registry/service/VsCodeService.java
+++ src/main/java/org/labmonkeys/che_vsx_registry/service/VsCodeService.java
@@ -3,11 +3,15 @@ package org.labmonkeys.che_vsx_registry.service;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.labmonkeys.che_vsx_registry.api.VsCodeApi;
 import org.labmonkeys.che_vsx_registry.dto.query.ExtensionQueryDto;
 import org.labmonkeys.che_vsx_registry.dto.query.FilterDto;
 import org.labmonkeys.che_vsx_registry.dto.query.FilterTypeDto;
+import org.labmonkeys.che_vsx_registry.dto.response.QueryResponseDto;
+import org.labmonkeys.che_vsx_registry.model.Extension;
+import org.labmonkeys.che_vsx_registry.model.ExtensionId;
 import org.labmonkeys.che_vsx_registry.service.utilities.QueryFlags;
 
 import jakarta.ws.rs.core.Response;
@@ -18,7 +22,9 @@ public class VsCodeService implements VsCodeApi {
     @Override
     public Response extensionQuery(ExtensionQueryDto query, UriInfo uriInfo) {
 
+        // The Base URI will be needed in the response data for the Asset URIs
         URI baseUri = uriInfo.getBaseUri();
+        List<QueryResponseDto> results = new ArrayList<>();
         QueryFlags flags = new QueryFlags(query.flags());
         for (FilterDto queryFilter : query.filters()) {
             List<String> extensionNameQueries = new ArrayList<String>();
@@ -39,7 +45,15 @@ public class VsCodeService implements VsCodeApi {
                         break;
                 }
             }
-
+            if (!extensionNameQueries.isEmpty()) {
+                findByName(extensionNameQueries, flags);
+            }
+            if (!searchQueries.isEmpty()) {
+                findByText(searchQueries, flags);
+            }
+            if (!extensionIdQueries.isEmpty()) {
+                findById(extensionIdQueries, flags);
+            }
         }
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'extensionQuery'");
@@ -51,5 +65,36 @@ public class VsCodeService implements VsCodeApi {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getBlob'");
     }
+
+    private QueryResponseDto findByName(List<String> extensionNames, QueryFlags flags) {
+
+        for (String extensionName : extensionNames.stream().distinct().collect(Collectors.toList())) {
+            String[] tokens = extensionName.split(".");
+            // Throw exception if tokens.length != 2
+            String publisher = tokens[0];
+            String name = tokens[1];
+            ExtensionId id = new ExtensionId(publisher , name);
+            Extension extension = Extension.findById(id);
+        }
+        return null;
+    }
+
+    private QueryResponseDto findByText(List<String> searchText, QueryFlags flags) {
+        for(String text : searchText.stream().distinct().collect(Collectors.toList())) {
+
+        }
+        return null;
+    }
+
+    private QueryResponseDto findById(List<String> extensionIds, QueryFlags flags) {
+        for (String extensionId : extensionIds.stream().distinct().collect(Collectors.toList())) {
+                    
+        }
+        return null;
+    }
+
+    
+
+
     
 }
